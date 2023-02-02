0ab51d4968b151b94026cfe3bb81b3ca944dbcaa
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-02-02 10:39:39 -0300

    feat: create failed requests queue and run after get new token

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,13 @@ import axios, { AxiosError } from "axios";
 import { setCookie, parseCookies } from "nookies";
 
 let cookies = parseCookies();
+let isRefreshing = false;
+
+type FailedRequest = {
+  onSuccess: (token: string) => void;
+  onFailure: (error: AxiosError) => void;
+};
+let failedRequestsQueue: FailedRequest[] = [];
 
 export const api = axios.create({
   baseURL: "http://localhost:3333",
@@ -24,30 +31,54 @@ api.interceptors.response.use(
         cookies = parseCookies();
 
         const { "auth-example.refreshToken": refreshToken } = cookies;
+        const originalConfig = error.config;
 
-        api
-          .post("/refresh", {
-            refreshToken,
-          })
-          .then(response => {
-            const { token } = response.data;
+        if (!isRefreshing) {
+          isRefreshing = true;
+          api
+            .post("/refresh", {
+              refreshToken,
+            })
+            .then(response => {
+              const { token } = response.data;
 
-            setCookie(undefined, "auth-example.token", token, {
-              maxAge: 60 * 60 * 24 * 30, // 30 days
-              path: "/",
-            });
-            setCookie(
-              undefined,
-              "auth-example.refreshToken",
-              response.data.refreshToken,
-              {
+              setCookie(undefined, "auth-example.token", token, {
                 maxAge: 60 * 60 * 24 * 30, // 30 days
                 path: "/",
-              },
-            );
+              });
+              setCookie(
+                undefined,
+                "auth-example.refreshToken",
+                response.data.refreshToken,
+                {
+                  maxAge: 60 * 60 * 24 * 30, // 30 days
+                  path: "/",
+                },
+              );
 
-            api.defaults.headers["Authorization"] = `Bearer ${token}`;
+              api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+              failedRequestsQueue.forEach(request => request.onSuccess(token));
+              failedRequestsQueue = [];
+            })
+            .catch(error => {
+              failedRequestsQueue.forEach(request => request.onFailure(error));
+              failedRequestsQueue = [];
+            })
+            .finally(() => (isRefreshing = false));
+        }
+
+        return new Promise((resolve, reject) => {
+          failedRequestsQueue.push({
+            onSuccess: (token: string) => {
+              if (!originalConfig?.headers) return;
+              originalConfig.headers["Authorization"] = `Bearer ${token}`;
+
+              resolve(api(originalConfig));
+            },
+            onFailure: (error: AxiosError) => reject(error),
           });
+        });
       } else {
       }
     }
