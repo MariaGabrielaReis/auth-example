fc303329ef2ee57eeea0bb023985fffd67303b1c
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-02-02 10:13:45 -0300

    feat: use refresh token to get new valid token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
-import axios from "axios";
-import { parseCookies } from "nookies";
+import axios, { AxiosError } from "axios";
+import { setCookie, parseCookies } from "nookies";
 
-const cookies = parseCookies();
+let cookies = parseCookies();
 
 export const api = axios.create({
   baseURL: "http://localhost:3333",
@@ -9,3 +9,47 @@ export const api = axios.create({
     Authorization: `Bearer ${cookies["auth-example.token"]}`,
   },
 });
+
+interface AxiosErrorResponse {
+  code?: string;
+}
+
+api.interceptors.response.use(
+  response => {
+    return response;
+  },
+  (error: AxiosError<AxiosErrorResponse>) => {
+    if (error.response?.status === 401) {
+      if (error.response.data?.code === "token.expired") {
+        cookies = parseCookies();
+
+        const { "auth-example.refreshToken": refreshToken } = cookies;
+
+        api
+          .post("/refresh", {
+            refreshToken,
+          })
+          .then(response => {
+            const { token } = response.data;
+
+            setCookie(undefined, "auth-example.token", token, {
+              maxAge: 60 * 60 * 24 * 30, // 30 days
+              path: "/",
+            });
+            setCookie(
+              undefined,
+              "auth-example.refreshToken",
+              response.data.refreshToken,
+              {
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                path: "/",
+              },
+            );
+
+            api.defaults.headers["Authorization"] = `Bearer ${token}`;
+          });
+      } else {
+      }
+    }
+  },
+);
